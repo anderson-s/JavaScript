71e513b6040392fcbf13f89b59a10035a2015505
Andeson Silva <andsilva607@gmail.com>
2022-03-20 12:07:07 -0300

    Js_para_iniciantes

---
+++ Js_para_iniciantes/aula03_constantes/index.js
@@ -0,0 +1,10 @@
+//Não podemos criar constantes com palavras reservadas pelo JS
+//contantes precisam ter nomes significativos, pois facilita o significado delas em aplicações
+//Não pode começar o nome de uma constante com número
+//Elas não podem conter espaços ou traços
+//Utilizamos camelCase, ou seja, contantes compostas começam com letra minuscula e a segunda parte com maiuscula
+//Case-sensitive --> o tamanho das letras importam
+//Não pode modificar o valor de uma constante
+//Não utilize VAR, Utilize const
+//Deve inserir um valor quando ela for declarada
+const nome = "Andeson";
